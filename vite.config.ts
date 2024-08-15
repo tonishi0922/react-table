26a5e2d4bf60f144951eb11ac354ed6bd302c9f5
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-08-15 17:58:04 +0900

    add: path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import kumaUI from "@kuma-ui/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), kumaUI()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
