8d28b116a6c454f4bdcd9a16212992da011191c6
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-08-16 22:01:38 +0900

    add: App.tsxの整理、sb導入前の整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,18 @@
-import { testData, testColumns, Columns, Data } from "./fixture/testData";
+import { testData, testColumns, Data } from "./fixture/testData";
+import { Columns } from "./types/types";
 
-import { Flex, Box } from "@kuma-ui/core";
 import { useState } from "react";
 
+import ColumnHeader from "./components/ColumnHeader";
+import ColumnContents from "./components/ColumnContents";
+
 function App() {
-  const [columns, setColumn] = useState<Columns>(testColumns);
+  const [columns, setColumns] = useState<Columns>(testColumns);
   const [data, setData] = useState<Data>(testData);
   return (
     <>
-      <Flex>
-        {columns.map((column, index) => {
-          return (
-            <div key={index}>
-              <Box
-                width={column.width}
-                display={"flex"}
-                justifyContent={"center"}
-                bg={"#F5F5F5"}
-                border={"1px solid gray"}
-                m={4}
-                onInput={(e: React.ChangeEvent<HTMLDivElement>) => {
-                  const tmpColumn = [...columns];
-                  tmpColumn[index].name = e.target.innerHTML;
-                  setColumn(testColumns);
-                }}
-                contentEditable={true}
-                suppressContentEditableWarning={true}
-                dangerouslySetInnerHTML={{ __html: column.name }}
-              ></Box>
-            </div>
-          );
-        })}
-      </Flex>
-      {data.map((item, itemIndex) => {
-        return (
-          <div key={itemIndex}>
-            <Flex>
-              {columns.map((column, columnIndex) => {
-                return (
-                  <Box
-                    key={columnIndex}
-                    width={column.width}
-                    display={"flex"}
-                    justifyContent={"center"}
-                    bg={columnIndex === 0 ? "#F5F5F5" : ""}
-                    m={4}
-                    border={"1px solid gray"}
-                    onInput={(e: React.ChangeEvent<HTMLDivElement>) => {
-                      const tmpData = [...data];
-                      tmpData[itemIndex][column.name] = e.target.innerHTML;
-                      setData(tmpData);
-                    }}
-                    contentEditable={true}
-                    suppressContentEditableWarning={true}
-                    dangerouslySetInnerHTML={{ __html: item[column.name] }}
-                  ></Box>
-                );
-              })}
-            </Flex>
-          </div>
-        );
-      })}
+      <ColumnHeader columns={columns} onInputHandler={setColumns} />
+      <ColumnContents columns={columns} data={data} onInputHandler={setData} />
     </>
   );
 }
