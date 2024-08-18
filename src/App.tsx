9592960c8ca73d11b006b21c6c1a1505e8557017
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-08-18 11:15:02 +0900

    add: lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { testData, testColumns, Data } from "./fixture/testData";
-import { Columns } from "./types/types";
+import { testData, testColumns } from "./fixture/testData";
 
-import { useState } from "react";
-
-import ColumnHeader from "./components/ColumnHeader";
-import ColumnContents from "./components/ColumnContents";
 import Table from "./components/Table";
 
 function App() {
-  const [columns, setColumns] = useState<Columns>(testColumns);
-  const [data, setData] = useState<Data>(testData);
   return (
     <>
-      <Table columns={columns} data={data} margin={24} />
+      <Table columns={testColumns} data={testData} margin={24} />
     </>
   );
 }
