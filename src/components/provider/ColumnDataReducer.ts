a7fad4b5a6434e75133bdc490416045c01384e84
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-08-20 23:42:59 +0900

    add: ColumnDataReducer作成

--- src/components/provider/ColumnDataReducer.ts
+++ src/components/provider/ColumnDataReducer.ts
@@ -1,15 +1,28 @@
-import { Columns } from "../../types/types";
-import { defaultColumnData } from "../../lib/util";
+import { Column, Columns } from "../../types/types";
+import { defaultColumn } from "../../lib/util";
+import { Dispatch, createContext, useContext } from "react";
 type Index = number;
 
 export type Action =
   | { type: "ADD"; payload: Index }
-  | { type: "DELETE"; payload: Index };
+  | { type: "DELETE"; payload: Column };
 
 export const columnsReducer = (columns: Columns, action: Action): Columns => {
   const { type, payload } = action;
   switch (type) {
     case "ADD":
+      const index = payload;
+      const id = Math.random() as unknown as string;
+      const insertColumn: Column = { ...defaultColumn, id };
+      console.log(insertColumn, "insertColumn");
+      return [
+        ...columns.slice(0, index),
+        insertColumn,
+        ...columns.slice(index),
+      ];
   }
   return columns;
 };
+
+const ColumnsDispatchContext = createContext<Dispatch<Action>>(() => undefined);
+export const useColumnsDispatch = () => useContext(ColumnsDispatchContext);
