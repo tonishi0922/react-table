3055a2a858ea2612b1597805ddfdb43c6cf334ca
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-08-24 23:02:49 +0900

    add: lint rule

--- eslint.config.js
+++ eslint.config.js
@@ -22,5 +22,11 @@ export default tseslint.config({
       "warn",
       { allowConstantExport: true },
     ],
+    "no-implicit-coercion": [
+      "warn",
+      {
+        boolean: true,
+      },
+    ],
   },
 });
