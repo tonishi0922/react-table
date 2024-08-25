b3a7fb12d10e092ff276949b3ad154f575e295e8
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-08-25 14:00:17 +0900

    add: RowDataReducer

---
+++ src/components/provider/RowDataReducer.ts
@@ -0,0 +1,30 @@
+import { DataItem, Data } from "../../types/types";
+import { Dispatch, createContext, useContext } from "react";
+import { useColumns } from "./ColumnDataContext";
+type Index = number;
+
+export type Action =
+  | { type: "ADD"; payload: Index }
+  | { type: "DELETE"; payload: Data };
+
+export const dataReducer = (data: Data, action: Action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "ADD": {
+      const columns = useColumns();
+      const insertRowData: DataItem = columns.reduce((acc, column) => {
+        acc[column.value] = "";
+        return acc;
+      }, {} as DataItem);
+      const index = payload;
+      return [...data.slice(0, index), insertRowData, ...data.slice(index)];
+    }
+    default:
+      throw new Error("Invalid action");
+  }
+};
+
+export const DataDispatchContext = createContext<Dispatch<Action>>(
+  () => undefined
+);
+export const useDataDispatch = () => useContext(DataDispatchContext);
