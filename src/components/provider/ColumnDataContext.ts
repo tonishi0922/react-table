3bffccfea7e49c85c181ba14f606240b7ef1abd9
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-08-26 21:50:09 +0900

    delete: ColumnとRow専用のProvider削除

--- src/components/provider/ColumnDataContext.ts
+++
@@ -1,7 +0,0 @@
-/* eslint react-refresh/only-export-components: 0 */
-import { Columns } from "../../types/types";
-import { defaultColumns } from "../../lib/util";
-import { createContext, useContext } from "react";
-
-export const columnContext = createContext<Columns>(defaultColumns);
-export const useColumns = () => useContext(columnContext);
--- src/components/provider/RowDataContext.ts
+++
@@ -1,7 +0,0 @@
-/* eslint react-refresh/only-export-components: 0 */
-import { Data } from "../../types/types";
-import { defaultData } from "../../lib/util";
-import { createContext, useContext } from "react";
-
-export const dataContext = createContext<Data>(defaultData);
-export const useData = () => useContext(dataContext);
