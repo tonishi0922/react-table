157c53292dbc282143fe838b98640e71487dfa48
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-08-28 23:36:39 +0900

    update: tableReducerのadd_columnの処理でdataが追加されるように変更

--- src/components/provider/TableDataReducer.ts
+++ src/components/provider/TableDataReducer.ts
@@ -1,13 +1,15 @@
 /* eslint react-refresh/only-export-components: 0 */
-import { Column, Columns, TableData, DataItem } from "../../types/types";
+import { Column, Columns, TableData, DataItem, Data } from "../../types/types";
 import { defaultColumn } from "../../lib/util";
 import { Dispatch, createContext, useContext } from "react";
 type Index = number;
 
 export type Action =
   | { type: "ADD_COLUMN"; payload: Index }
-  | { type: "ADD_DATA"; payload: Index }
+  | { type: "SET_COLUMN"; payload: { index: Index; value: string } }
   | { type: "DELETE_COLUMN"; payload: TableData }
+  | { type: "ADD_DATA"; payload: Index }
+  | { type: "SET_DATA"; payload: { index: Index; value: string } }
   | { type: "DELETE_DARA"; payload: TableData };
 
 export const tableDataReducer = (
@@ -20,20 +22,38 @@ export const tableDataReducer = (
     case "ADD_COLUMN": {
       const index = payload;
       const id = Math.random() as unknown as string;
-      const insertColumn: Column = { ...defaultColumn, id };
+      const value = Math.random() as unknown as string;
+      const insertColumn: Column = { ...defaultColumn, id, value };
       const newColumns: Columns = [
         ...columns.slice(0, index),
         insertColumn,
         ...columns.slice(index),
       ];
-      return { columns: newColumns, data };
+      const insertColumnRowData: Data = data.map((row) => {
+        return { ...row, [id]: "" };
+      });
+      return { columns: newColumns, data: insertColumnRowData };
     }
     case "ADD_DATA": {
+      const index = payload;
       const insertRowData: DataItem = columns.reduce((acc, column) => {
         acc[column.value] = "";
         return acc;
       }, {} as DataItem);
-      const index = payload;
+      console.log(data);
+      const newData = [
+        ...data.slice(0, index),
+        insertRowData,
+        ...data.slice(index),
+      ];
+      return { columns, data: newData };
+    }
+    case "SET_DATA": {
+      const { index, value } = payload;
+      const insertRowData: DataItem = columns.reduce((acc, column) => {
+        acc[column.value] = "";
+        return acc;
+      }, {} as DataItem);
       const newData = [
         ...data.slice(0, index),
         insertRowData,
